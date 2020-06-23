969018fc429f053da9b861e64c8721df6aa6726c
Riley Shaw <contact@shiney.dev>
2020-06-23 09:20:30 +0100

    refactor intentionalpain.asm

--- x86-64/intentionalpain.asm
+++ x86-64/intentionalpain.asm
@@ -1,55 +1,45 @@
 ; ---------------------------------------------------------------------
+; intentionalpain.asm
 ;
-; An x86-64 assembly *something* for having a bad time
+; This is an x86-64 assembly program for having a bad time.
 ;
+; nasm -f elf64 -o write.o write.asm
 ; nasm -f elf64 -o intentionalpain.o intentionalpain.asm
-;
+; ld -o intentionalpain intentionalpain.o write.o
+; ./intentionalpain
 ; ---------------------------------------------------------------------
 
-%define stdout 1
-%define stderr 2
-
-%define newline 10
-
-section .data                                       ; riveting declarations
-
-    error_message db "Uh-oh!", newline              ; error_message = b"Uh-oh!\n"
-    error_message_length equ $-error_message        ; thank u nasm, error_message_length = len(error_message)
-
-    success_message db "Have a nice day!", newline
-    success_message_length equ $-success_message
+    global  _start
+    extern  write
 
 section .text
 
-global _start
 _start:
-    rdseed  r8                                      ; r8 = random.seed()
-    mov     r9, r8
-    and     r9, 01                                  ; r9 = r8[0] - only get the first bit
+    rdseed  r8                         ; r8 = random.seed()
+    and     r8, 1b                     ; get the first bit
 
-    mov     rax, 1                                  ; tell linux to get ready to write something
-    cmp     r9, 0                                   ; cmp = bool(r9)
+    test    r8, r8                     ; zf = ~bool(r8)
+    jnz     _start_good                ; if zf == 1: jmp _start_good
+_start_bad:
+    mov     rdi, 2                     ; write to stderr just to be a nuisance
+    mov     rsi, bad_msg               ; put the location of what we're writing into a register
+    mov     rdx, bad_msg_l             ; now tell syscall how many bits we're writing
+    call    write
 
-    jz      _bad_day                                ; if cmp == 0: _bad_day()
-    jmp     _good_day                               ; else: _good_day()
+    hlt                                ; we are not a kernel -> fast SIGSEGV :)
+_start_good:
+    mov     rdi, 1                     ; write to stdout
+    mov     rsi, good_msg              ; put the location of what we're writing into a register
+    mov     rdx, good_msg_l            ; now tell syscall how many bits we're writing
+    call    write
 
-_bad_day:
-    mov     rdi, stderr                             ; write to stderr just to be a nuisance
-    mov     rsi, error_message                      ; put the location of what we're writing into a register
-    mov     rdx, error_message_length               ; now tell linux how many bytes we're writing
-    syscall                                         ; actually do it lol
+    mov     rax, 60                    ; exit(
+    mov     rdi, 0                     ;   0,
+    syscall                            ; )
 
-    jmp _purgatory                                  ; to the abyss we go
+section .data
 
-_good_day:
-    mov     rdi, stdout                             ; write to stdout
-    mov     rsi, success_message                    ; put the location of what we're writing into a register
-    mov     rdx, success_message_length             ; now tell linux how many bytes we're writing
-    syscall
-
-    mov     rax, 60                                 ; syscall for exit
-    xor     rdi, rdi
-    syscall                                         ; because we're nice, exit without segfaulting
-
-_purgatory:
-    mov     rax, 1                                  ; mmm segfault
+bad_msg:    db      "Uh-oh!", 10
+bad_msg_l:  equ     $-bad_msg
+good_msg:   db      "Have a nice day!", 10
+good_msg_l: equ     $-good_msg
